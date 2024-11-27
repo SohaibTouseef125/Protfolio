33f1f6d9f10f496307552039e2b421c6c9f6734b
Sohaib Touseef <muhammadsohaib2233344@gmail.com>
2024-11-27 18:32:16 +0500

    MY Protfolio

---
+++ SohaibProtfolio/src/app/data.js
@@ -0,0 +1,147 @@
+/*
+Websites:
+
+- https://github.com/pmndrs/gltfjsx (GLTF JSX for 3D Models)
+- https://lucide.dev/icons/ (Lucide Icons)
+- https://github.com/anuraghazra/github-readme-stats (Github Readme Stats)
+- https://skillicons.dev (Skill Icons to show skills)
+- https://github-readme-streak-stats.herokuapp.com (Github Readme Streak Stats)
+
+:root {
+  --background: 27 27 27;
+  --foreground: 225 225 225;
+  --muted: 115 115 115;
+  --accent: 254 254 91; #FEFE5B
+}
+
+*/
+
+export const projectsData = [
+  {
+    id: 1,
+    name: "Resume-Builder",
+    description: "A web-based tool  users to create professional Resume-Builder",
+    date: "2024-08-15",
+    demoLink: "https://milestone5-eosin.vercel.app/",
+  },
+  {
+    id: 2,
+    name: "Resume-Builder Editable",
+    description: "A web-based users to create professional Resume-Builder Editable",
+    date: "2024-08-15",
+    demoLink: "https://milestone4-six-sable.vercel.app/",
+  },
+  {
+    id: 3,
+    name: "Governor-initiatives",
+    description: "An official website showcasing the Governor of Sindh’s initiatives",
+    date: "2024-08-14",
+    demoLink: "https://govenor-sindh-website.vercel.app/",
+  },
+  {
+    id: 4,
+    name: "Countdown-Timer",
+    description: "Simple and timer application that countsdown to  specified time or event.",
+    date: "2024-08-14",
+    demoLink: "https://30days-30-projects-next-js.vercel.app/",
+  },
+  {
+    id: 5,
+    name: "Fimga Design-Website",
+    description: "A pixel-perfect website design in optimized for user experience",
+    date: "2024-08-12",
+    demoLink: "https://fimga-design-website.vercel.app/",
+  },
+  {
+    id: 6,
+    name: "Calculator Dynamic",
+    description: "A calculator application with real-time input handling and multi-device",
+    date: "2024-02-11",
+    demoLink: "https://multiple-projects-dynamic.vercel.app/",
+  },
+ 
+  {
+    id: 7,
+    name: "Digital-Clock",
+    description:"digital clock real-time hours, minutes, and seconds with styles formats.",
+    date: "2024-08-10",
+    demoLink: "https://digital-clock-dynamic.vercel.app/",
+  },
+  {
+    id: 8,
+    name: "Simple Protfolio",
+    description: "portfolio showcasing personal projects, skills, and with a responsive design.",
+    date: "2024-08-10",
+    demoLink: "https://simple-protfolio-html-css.vercel.app/",
+  },
+  {
+    id: 9,
+    name: "Simple Website",
+    description: "Next.js website with optimized  server-side rendering for navigation.",
+    date: "2024-08-10",
+    demoLink: "https://nextjs-website-tailwind-css-theta.vercel.app/",
+  },
+  {
+    id: 10,
+    name: "Simple Protfolio",
+    description: "Portfolio website featuring smooth for and appealing user experience.",
+    date: "2024-08-9",
+    demoLink: "https://multiple-projects-dynamic-3flq.vercel.app/",
+  },
+  {
+    id: 11,
+    name: "Blub",
+    description: "bulb project that toggles light on and off with a click,functionality.",
+    date: "2024-08-9",
+    demoLink: "https://dynamic-blub.vercel.app/",
+  },
+  
+  
+ 
+  
+  
+
+ 
+
+ 
+];
+
+export const BtnList = [
+  { label: "Home", link: "/", icon: "home", newTab: false },
+  { label: "About", link: "/about", icon: "about", newTab: false },
+  { label: "Projects", link: "/projects", icon: "projects", newTab: false },
+  { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+  {
+    label: "Github",
+    link: "https://github.com/SohaibTouseef125",
+    icon: "github",
+    newTab: true,
+  },
+  {
+    label: "LinkedIn",
+    link: "https://www.linkedin.com/in/sohaib-touseef-12a7b628a/",
+    icon: "linkedin",
+    newTab: true,
+  },
+  {
+    label: "Facebook",
+    link: "https://www.facebook.com/SohaibTouseef",
+    icon: "facebook",
+    newTab: true,
+  },
+  {
+    label: "Instagram",
+    link: "https://www.instagram.com/",
+    icon: "instagram",
+    newTab: true,
+  },
+
+  {
+    label: "Resume",
+    link: "/resume.pdf",
+    icon: "resume",
+    newTab: true,
+  },
+];
+
+
